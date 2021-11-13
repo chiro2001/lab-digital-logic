aa9d2a97af4cf8243d8c6a42da7d40acd106f751
Chiro <Chiro2001@163.com>
2021-11-13 16:14:08 +0800

    lab02 submit chisel

--- xc7z020/xc7z020.sim/sim_1/behav/xsim/xsim.dir/module_testbench_behav/webtalk/xsim_webtalk.tcl
+++ xc7z020/xc7z020.sim/sim_1/behav/xsim/xsim.dir/module_testbench_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir D:/Programs/lab-digital-logic/xc7z020/xc7z020.sim/sim_1/behav/xsim/xsim.dir/module_testbench_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Fri Nov 12 23:09:59 2021" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Sat Nov 13 16:13:54 2021" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.3 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2405991" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "24704bd7-ed27-473e-8070-c0bfa7048571" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "8be0637d68414a4182a9097ba2370a84" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "76" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "77" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "AMD Ryzen 7 4800U with Radeon Graphics         " -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "810 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.03_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "7292_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 1984783634 -regid "" -xml D:/Programs/lab-digital-logic/xc7z020/xc7z020.sim/sim_1/behav/xsim/xsim.dir/module_testbench_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/Programs/lab-digital-logic/xc7z020/xc7z020.sim/sim_1/behav/xsim/xsim.dir/module_testbench_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/Programs/lab-digital-logic/xc7z020/xc7z020.sim/sim_1/behav/xsim/xsim.dir/module_testbench_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.05_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "7732_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 4156302233 -regid "" -xml D:/Programs/lab-digital-logic/xc7z020/xc7z020.sim/sim_1/behav/xsim/xsim.dir/module_testbench_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/Programs/lab-digital-logic/xc7z020/xc7z020.sim/sim_1/behav/xsim/xsim.dir/module_testbench_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/Programs/lab-digital-logic/xc7z020/xc7z020.sim/sim_1/behav/xsim/xsim.dir/module_testbench_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
